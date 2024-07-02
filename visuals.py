e60cb9b44168a2e5dcdb85898a73746c3ce957ed
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-07-02 20:21:18 +0530

    added files via upload

---
+++ visuals.py
@@ -0,0 +1,155 @@
+###########################################
+# suppress matplotlib user warnings
+# necessary for newer version of matplotlib
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+
+# display inline matplotlib plots with IPython
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+###########################################
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from time import time
+from sklearn.metrics import f1_score, accuracy_score
+
+
+def distribution(data, feature_label, transformed = False):
+    """
+    visualization code for displaying skewed distributions of features
+    """
+    
+    sns.set_theme()
+    sns.set_style("whitegrid")
+    # create figure
+    fig = plt.figure(figsize = (20,5));
+
+    # skewed feature plotting
+    for i, feature in enumerate([feature_label]):
+        ax = fig.add_subplot(1, 2, i+1)
+        ax.hist(data[feature], bins = 20, color = '#2a8edb')
+        ax.set_title("'%s' Feature Distribution"%(feature), fontsize = 10)
+        ax.set_xlabel(feature_label)
+        ax.set_ylabel("Total Number")
+        
+        # red wine
+        ax.set_ylim((0, 800))
+        ax.set_yticks([0, 200, 400, 600, 800])
+        ax.set_yticklabels([0, 200, 400, 600, 800])
+        
+        # white wine 
+        #ax.set_ylim((0, 2500))
+        #ax.set_yticks([0, 500, 1000, 1500, 2000, 2500])
+        #ax.set_yticklabels([0, 500, 1000, 1500, 2000, 2500])
+        
+    # plot aesthetics
+    if transformed:
+        fig.suptitle("Log-Transformed Distributions", \
+            fontsize = 15, y = 1)
+    else:
+        fig.suptitle("Skewed Distributions", \
+            fontsize = 15, y = 1)
+
+    fig.tight_layout()
+    fig.show()
+
+
+def visualize_classification_performance(results):
+    """
+    visualization code to display results of various learners.
+    
+    inputs:
+      - results: a list of dictionaries of the statistic results from 'train_predict_evaluate()'
+    """
+  
+    # create figure
+    sns.set_theme()
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots(2, 3, figsize = (15,10))
+    # print("VERSION:")
+    # print(matplotlib.__version__)
+    # constants
+    bar_width = 0.3
+    colors = ["#e55547", "#4e6e8e", "#2ecc71"]
+    
+    # super loop to plot four panels of data
+    for k, learner in enumerate(results.keys()):
+        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
+            for i in np.arange(3):
+                
+                # creative plot code
+                ax[j//3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
+                ax[j//3, j%3].set_xticks([0.45, 1.45, 2.45])
+                ax[j//3, j%3].set_xticklabels(["1%", "10%", "100%"])
+                ax[j//3, j%3].set_xlabel("Training Set Size")
+                ax[j//3, j%3].set_xlim((-0.1, 3.0))
+    
+    # add unique y-labels
+    ax[0, 0].set_ylabel("Time (in seconds)")
+    ax[0, 1].set_ylabel("Accuracy Score")
+    ax[0, 2].set_ylabel("F-score")
+    ax[1, 0].set_ylabel("Time (in seconds)")
+    ax[1, 1].set_ylabel("Accuracy Score")
+    ax[1, 2].set_ylabel("F-score")
+    
+    # add titles
+    ax[0, 0].set_title("Model Training")
+    ax[0, 1].set_title("Accuracy Score on Training Subset")
+    ax[0, 2].set_title("F-score on Training Subset")
+    ax[1, 0].set_title("Model Predicting")
+    ax[1, 1].set_title("Accuracy Score on Testing Set")
+    ax[1, 2].set_title("F-score on Testing Set")
+    
+    # add horizontal lines for naive predictors
+    ax[0, 1].axhline(y = 1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[1, 1].axhline(y = 1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[0, 2].axhline(y = 1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[1, 2].axhline(y = 1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    
+    # set y-limits for score panels
+    ax[0, 1].set_ylim((0, 1))
+    ax[0, 2].set_ylim((0, 1))
+    ax[1, 1].set_ylim((0, 1))
+    ax[1, 2].set_ylim((0, 1))
+
+    # create patches for the legend
+    patches = []
+    for i, learner in enumerate(results.keys()):
+        patches.append(mpatches.Patch(color = colors[i], label = learner))
+    plt.legend(handles = patches, bbox_to_anchor = (-.80, 2.53), \
+               loc = 'upper center', borderaxespad = 0., ncol = 3, fontsize = 'x-large')
+    
+    # aesthetics
+    plt.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.10)
+    plt.tight_layout(pad=1, w_pad=2, h_pad=5.0)
+    plt.show()
+    
+
+def feature_plot(importances, X_train, y_train):
+    
+    # display the five most important features
+    indices = np.argsort(importances)[::-1]
+    columns = X_train.columns.values[indices[:11]]
+    values = importances[indices][:11]
+
+    sns.set_theme()
+    sns.set_style("whitegrid")
+
+    # creat the plot
+    fig = plt.figure(figsize = (12,5))
+    plt.title("Normalized Weights for First Five Most Predictive Features", fontsize = 15)
+    plt.bar(np.arange(11), values, width = 0.2, align="center", label = "Feature Weight")
+    # plt.bar(np.arange(11) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0',label = "Cumulative Feature Weight")
+    plt.xticks(np.arange(11), columns)
+    plt.xlim((-0.5, 4.5))
+    plt.ylabel("Weight", fontsize = 12)
+    plt.xlabel("Feature", fontsize = 12)
+    
+    plt.legend(loc = 'upper center')
+    plt.tight_layout()
+    plt.show() 
